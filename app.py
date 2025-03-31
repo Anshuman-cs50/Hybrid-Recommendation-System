56cec8b6d32ee4c59594aa50ac4a4dcefe8e5472
Anshuman-cs50 <anshumansingh0802.gmail.com>
2025-03-31 17:03:06 +0530

    added url_for import to app.py for URL generation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, url_for
 import pymysql
 
 app = Flask(__name__)
@@ -22,43 +22,39 @@ def index():
 
     # most viewed movies
     most_viewed = fetch_most_viewed_movies()
-    
+
     # get the most popular movies from the database
     cursor.execute("SELECT * FROM movies ORDER BY popularity DESC LIMIT 10")
-
 
     # get some genre-based recommendations
     return render_template('index.html', most_viewed=most_viewed)
 
 def fetch_most_viewed_movies():
-    cursor.execute("select movie_id, title, release_date, overview, poster_path from movies order by vote_count desc limit 2")
+    cursor.execute("select movie_id, title, release_date, overview, poster_path from movies order by vote_count desc limit 21")
     most_viewed = cursor.fetchall()
 
     # Convert the result to a list of dictionaries
     most_viewed = [
         {
             'movie_id': row[0],
-            'title': row[1],
-            'release_date': row[2],
-            'overview': row[3],
             'poster_path': row[4]
         } for row in most_viewed
     ]
 
     # helper function to fetch genres for a given movie_id
-    def fetch_genres(movie_id):
-        cursor.execute("""
-                        select name from genres 
-                        where id in (
-                            select genre_id from movie_genres where movie_id = %s
-                        )
-                    """, (movie_id, ))
-        genres = cursor.fetchall()
-        return [genre[0] for genre in genres] # return a flatten list of genres
+    # def fetch_genres(movie_id):
+    #     cursor.execute("""
+    #                     select name from genres 
+    #                     where id in (
+    #                         select genre_id from movie_genres where movie_id = %s
+    #                     )
+    #                 """, (movie_id, ))
+    #     genres = cursor.fetchall()
+    #     return [genre[0] for genre in genres] # return a flatten list of genres
 
-    # add attrifute genres to each movie in most_viewed
-    for movie in most_viewed:
-        movie["genres"] = fetch_genres(movie["movie_id"])
+    # # add attrifute genres to each movie in most_viewed
+    # for movie in most_viewed:
+    #     movie["genres"] = fetch_genres(movie["movie_id"])
 
     return most_viewed
 
